5b0152700319f664a86f737bf09bd372b7cdade7
CGD5709 <josevi585@gmail.com>
2026-02-16 16:24:08 +0100

    Errores corregidos

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -4,8 +4,6 @@ package acme.entities.sponsorships;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.validation.Valid;
 
@@ -48,12 +46,12 @@ public class Sponsorship extends AbstractEntity {
 
 	@Mandatory
 	//@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
-	@Temporal(TemporalType.TIMESTAMP)
+	//@Temporal(TemporalType.TIMESTAMP)
 	private Moment				startMoment;
 
 	@Mandatory
 	//@ValidMoment(constraint = future)
-	@Temporal(TemporalType.TIMESTAMP)
+	//@Temporal(TemporalType.TIMESTAMP)
 	private Moment				endMoment;
 
 	@Optional
