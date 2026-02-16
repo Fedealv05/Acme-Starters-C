3ff852309dd5d526e9157b9f6fba7e768f5bd12a
fedealv_05 <fedealvarezgarcia12@gmail.com>
2026-02-16 13:02:34 +0100

    Arreglo

---
+++ src/main/java/acme/entities/DonationRepository.java
@@ -0,0 +1,13 @@
+
+package acme.entities;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.client.repositories.AbstractRepository;
+
+public interface DonationRepository extends AbstractRepository {
+
+	@Query("SELECT SUM(d.money) FROM Donation d WHERE d.sponsorship.id = :sponsorshipId")
+	Double totalMoneyBySponsorshipId(int sponsorshipId);
+
+}
