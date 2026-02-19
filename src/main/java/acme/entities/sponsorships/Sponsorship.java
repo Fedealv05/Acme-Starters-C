8ac1388a7364c19cc72716818503dc68853de617
fedealv_05 <fedealvarezgarcia12@gmail.com>
2026-02-19 18:39:38 +0100

    Creado ValidTicker

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -16,6 +16,7 @@ import acme.client.components.datatypes.Moment;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidTicker;
 import acme.realms.Sponsor;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,7 +33,7 @@ public class Sponsorship extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	//@ValidTicker
+	@ValidTicker
 	@Column(unique = true)
 	private String				ticker;
 
