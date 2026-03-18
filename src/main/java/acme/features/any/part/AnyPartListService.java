4308be1bfb9f2faac39e76455f6529bd5fc8c2dd
CGD5709 <josevi585@gmail.com>
2026-03-18 16:42:33 +0100

    Correccion de restricciones para invenciones con partes vacías

--- src/main/java/acme/features/any/part/AnyPartListService.java
+++ src/main/java/acme/features/any/part/AnyPartListService.java
@@ -38,8 +38,8 @@ public class AnyPartListService extends AbstractService<Any, Part> {
 
 	@Override
 	public void authorise() {
-		boolean status;//comprobar que pasa si no hay partes
-		status = this.invention != null && this.parts.stream().allMatch(i -> i.getInvention().getDraftMode() == false);
+		boolean status;
+		status = this.invention != null && !this.invention.getDraftMode() && this.parts.stream().allMatch(i -> i.getInvention().getDraftMode() == false);
 		super.setAuthorised(status);
 	}
 
